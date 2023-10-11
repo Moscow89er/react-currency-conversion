fdf3aea59e2e1640548bf81b00f1286f3806b028
Nikita Khubaev <khubaev.n@yandex.ru>
2023-10-11 21:11:10 +0400

    working on rate section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import '../Converter/Converter';
 import Converter from '../Converter/Converter';
 import Header from '../Header/Header';
+import Rate from '../Rate/Rate';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Converter />
+      <Rate />
     </div>
   );
 }
--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -1,6 +1,18 @@
+import './Rate.css';
+
 function Rate () {
     return (
-        <section className="rate"></section>
+        <section className="rate d-flex flex-column">
+            <select name="currency" id="currency" className="rate-control form-control mb-3">
+                <option value="">Выберите валюту</option>
+                <option value="RUB">RUB</option>
+                <option value="GEL">GEL</option>
+                <option value="USD">USD</option>
+            </select>
+            <div className="rate-currency badge bg-primary mr-2 flex-grow-1">RUB 100</div>
+            <div className="rate-currency badge bg-success mr-2 flex-grow-1">GEL 2.7</div>
+            <div className="rate-currency badge bg-info mr-2 flex-grow-1">USD 1</div>
+        </section>
     )
 }
 

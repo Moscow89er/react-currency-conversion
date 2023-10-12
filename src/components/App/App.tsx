6c0a6de26c66893b78ebfa84763a7147c6234e9a
Nikita Khubaev <khubaev.n@yandex.ru>
2023-10-12 09:29:09 +0400

    working on react-router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import '../Converter/Converter';
 import Converter from '../Converter/Converter';
 import Header from '../Header/Header';
 import Rate from '../Rate/Rate';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Converter />
-      <Rate />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Converter />} />
+          <Route path='/rate' element={<Rate />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return (
         <header className="header bg-primary d-flex justify-content-center py-2">
-            <button className="header_button-link">Конвертация</button>
-            <button className="header_button-link">Курс</button>
+            <Link to='/' className="header_button-link">Конвертация</Link>
+            <Link to='/rate' className="header_button-link">Курс</Link>
         </header>
     )
 }
